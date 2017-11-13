0a6549abf1b6d64eb3fb12a9cb49ee9e7e9e10f7
Izzayoi <felixgiurgiuca@yahoo.com>
2017-11-13 19:48:05 +0100

    Button

--- app/src/main/java/com/example/felix/gasboard/MainActivity.java
+++ app/src/main/java/com/example/felix/gasboard/MainActivity.java
@@ -1,90 +1,36 @@
 package com.example.felix.gasboard;
-import android.bluetooth.BluetoothDevice;
-import android.content.Context;
-import android.os.Bundle;
+
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
-import android.view.View;
-import android.widget.Button;
-import android.widget.TextView;
+import android.os.Bundle;
 import android.widget.Toast;
 
-import Bluetooth.Bluetooth;
-
-public class MainActivity extends AppCompatActivity implements Bluetooth.CommunicationCallback
-{
-    Button b1;
-    Bluetooth bt;
-    TextView  tv;
-    CharSequence text = "";
+public class MainActivity extends AppCompatActivity {
 
     @Override
-    protected void onCreate(Bundle savedInstanceState)
-    {
-        //initialising activity
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
 
+    boolean doubleBackToExitPressedOnce = false;
 
-        //instantiating views
-        b1 = (Button) findViewById(R.id.button);
-        tv = (TextView) findViewById(R.id.textView);
+    @Override
+    public void onBackPressed() {
+        if (doubleBackToExitPressedOnce) {
+            super.onBackPressed();
+            return;
+        }
 
-        //set up debug toast. Change text before displaying message
-        Context context = getApplicationContext();
-        final Toast toast = Toast.makeText(context, text,Toast.LENGTH_SHORT);
+        this.doubleBackToExitPressedOnce = true;
+        Toast.makeText(this, "Please click BACK again to exit", Toast.LENGTH_SHORT).show();
 
-        //create onClick event handler
-        View.OnClickListener listen = new View.OnClickListener()
-        {
+        new Handler().postDelayed(new Runnable() {
+
             @Override
-            public void onClick(View v)
-            {
-                if(v.getId() == R.id.button)
-                {
-                    bt.send((byte)05);
-                    Log.d("MSG","I send something" );
-                }
+            public void run() {
+                doubleBackToExitPressedOnce=false;
             }
-        };
-        b1.setOnClickListener(listen);
-
-        // Set up blutooth connection
-        bt = new Bluetooth(this);
-        bt.enableBluetooth();
-        bt.setCommunicationCallback(this);
-        bt.connectToName("hc06");
-
-
-
-
-    }
-
-
-
-    @Override
-    public void onConnect(BluetoothDevice device) {
-
-    }
-
-    @Override
-    public void onDisconnect(BluetoothDevice device, String message) {
-
-    }
-
-    @Override
-    public void onMessage(int message) {
-        text = Integer.toString(message);
-        Log.d("MSG", (String) text);
-    }
-
-    @Override
-    public void onError(String message) {
-
-    }
-
-    @Override
-    public void onConnectError(BluetoothDevice device, String message) {
-
+        }, 2000);
     }
 }
